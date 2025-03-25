c377acbb4923f6d433bceb4e103a329180898e81
Vladyslav Ralets <vladralets@gmail.com>
2025-03-25 13:08:19 +0100

    added pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // additionalData: `@import "src/variables.scss";`
-      }
-    }
-  }
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+                cart: path.resolve(__dirname, 'cart.html'),
+                product: path.resolve(__dirname, 'product.html'),
+            },
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // additionalData: `@import "src/variables.scss";`
+            },
+        },
+    },
 });
