c8225b56fdbf38bbe0fc65da06b1d39bd9a4f98d
Vladyslav Ralets <vladralets@gmail.com>
2025-03-25 18:29:00 +0100

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
                 cart: path.resolve(__dirname, 'cart.html'),
                 product: path.resolve(__dirname, 'product.html'),
                 chat: path.resolve(__dirname, 'chat.html'),
+                cartTs: path.resolve(__dirname, 'src/cart.ts'),
             },
         },
     },
